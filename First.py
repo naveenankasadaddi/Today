b88f72adbdfc23a9605ec13a95d8bab26616adc4
Naveen <65064825+naveenankasadaddi@users.noreply.github.com>
2023-03-09 13:16:31 +0530

    Display the name and usn

--- First.py
+++ First.py
@@ -14,3 +14,6 @@ n=int(input("Enter the name :"))
 
 usn=input("Enter the usn in the string format :")
 >>>>>>> 4111ecd0913d301b67e215a53ec1716471ebcc7a
+
+
+print("Thank you for visiting the college {} with {}".format(n,usn))
